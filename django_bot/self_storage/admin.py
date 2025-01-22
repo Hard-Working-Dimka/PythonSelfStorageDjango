5f2dab99ad5ecf81e441aaca8d6d74fb6f35a2e6
Hard-Working-Dimka <152815674+Hard-Working-Dimka@users.noreply.github.com>
2025-01-22 13:59:40 +0300

    Add basic models

--- django_bot/self_storage/admin.py
+++ django_bot/self_storage/admin.py
@@ -1,7 +1,26 @@
 from django.contrib import admin
-from .models import Client
+from .models import (
+    Client, Warehouse, Plans, Order
+)
 
 
 @admin.register(Client)
 class ClientAdmin(admin.ModelAdmin):
-    list_display = ('name', 'phone')
+    list_display = ('name', 'phone', 'address_of_client')
+
+
+@admin.register(Warehouse)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('address', 'free_capacity')
+
+
+@admin.register(Plans)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('volume_of_cell', 'price')
+
+
+@admin.register(Order)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = (
+        'client', 'cell_address', 'days', 'size', 'delivery', 'days_left', 'key_of_cell', 'plan', 'items'
+    )
--- django_bot/self_storage/models.py
+++ django_bot/self_storage/models.py
@@ -1,4 +1,9 @@
 from django.db import models
+import random
+
+
+def generate_cell_key():
+    return random.randrange(100, 10000)
 
 
 # TODO: пользователь регается, и в своем ЛК можешь в любой момент вызвать курьера, например.
@@ -6,18 +11,7 @@ class Client(models.Model):
     name = models.CharField(max_length=50)
     phone = models.CharField(max_length=12)
     tg_id = models.BigIntegerField(unique=True)
-    address = models.CharField(max_length=50)  # для курьера
-
-
-# TODO: qr код должен быть уникальным для каждой посылки у каждого пользователя, т.к. посылок может быть несколько.
-#      Можно сделать поле с рандомным числом, и при запросе оно накладывается на картинку - шаблон.
-#      Добавить функцию отсчета дней
-class Order(models.Model):
-    days = models.IntegerField(max_length=10)
-    size = models.CharField(max_length=12)
-    delivery = models.CharField(max_length=12)  # TODO нужна ли доставка/замер, использовать в админке для развозки
-    days_left = models.CharField(max_length=12)  # TODO остаток дней до конца аренды (возможно лишнее, подумать)
-    key_of_cell = models.IntegerField(max_length=10)
+    address_of_client = models.CharField(max_length=50)  # для курьера
 
 
 # TODO: у каждого склада будет своя вместительность, когда пользователь выберет сколько нужно места, то список складов
@@ -25,10 +19,26 @@ class Order(models.Model):
 #       вывести на экран "уменьшите объем посылки и тп."
 class Warehouse(models.Model):
     address = models.CharField(max_length=30)
-    free_capacity = models.IntegerField(max_length=30)
-
-    # TODO тарифы на хранение, наверное, лишнее, подумать
+    free_capacity = models.IntegerField()
 
 
+# TODO тарифы на хранение, наверное, лишнее, подумать
 class Plans(models.Model):
-    pass
+    price = models.IntegerField()
+    volume_of_cell = models.IntegerField()
+
+
+# TODO: qr код должен быть уникальным для каждой посылки у каждого пользователя, т.к. посылок может быть несколько.
+#      Можно сделать поле с рандомным числом, и при запросе оно накладывается на картинку - шаблон.
+#      Добавить функцию отсчета дней
+class Order(models.Model):
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    cell_address = models.ForeignKey(Warehouse, on_delete=models.CASCADE)
+    plan = models.ForeignKey(Plans, on_delete=models.CASCADE)
+
+    days = models.IntegerField()
+    size = models.CharField(max_length=12)
+    delivery = models.BooleanField(max_length=12)  # TODO нужна ли доставка/замер, использовать в админке для развозки
+    days_left = models.CharField(max_length=12)  # TODO остаток дней до конца аренды (возможно лишнее, подумать)
+    key_of_cell = models.IntegerField(editable=False, default=generate_cell_key())
+    items = models.CharField(max_length=256)  # описание (что хранится)

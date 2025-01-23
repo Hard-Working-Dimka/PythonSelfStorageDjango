992933d8abf1b10ee5c030b38fc8449e761fc358
Hard-Working-Dimka <152815674+Hard-Working-Dimka@users.noreply.github.com>
2025-01-23 23:13:04 +0300

    add api

--- API/serializers.py
+++ API/serializers.py
@@ -7,22 +7,30 @@ from storage.models import (
 class CustomUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
-        fields = '__all__'
+        fields = ('id', 'username', 'telegram_id', 'telegram_name')
 
 
 class StorageSerializer(serializers.ModelSerializer):
     class Meta:
         model = Storage
-        fields = '__all__'
+        fields = ('id', 'name', 'location', 'max_capacity')
 
 
 class StorageUnitSerializer(serializers.ModelSerializer):
     class Meta:
         model = StorageUnit
-        fields = '__all__'
+        fields = ('id', 'storage', 'unit_number', 'size', 'is_occupied')
 
 
 class OrderSerializer(serializers.ModelSerializer):
+    customer = CustomUserSerializer()
+    storage_unit = StorageUnitSerializer()
+    storage = StorageSerializer()
+
     class Meta:
         model = Order
-        fields = '__all__'
+        depth = 1
+        fields = (
+            'id', 'customer', 'storage_unit', 'storage', 'address', 'phone_number', 'full_name', 'start_date', 'end_date',
+            'status', 'qr_issued'
+        )
